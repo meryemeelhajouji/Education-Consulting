b01a9c71b9088732452d357c565022b5b6b6ef5c
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2023-03-03 09:09:21 +0100

    add packae cryptage & jwt

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,6 +1,9 @@
 const dotenv = require("dotenv");
 const User = require("../models/User");
 const Role = require("../models/Role");
+const bcrypt=require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const { restart } = require("nodemon");
 
 // method  : post
 // url     : api/auth/login
@@ -13,47 +16,63 @@ const login = async (req, res) => {
 // route : api/auth/register
 // acces : Public
 const register = async (req, res) => {
-  if (!req.body.email || !req.body.name || !req.body.password) {
-    res.status(400).send(" please enter email or name or password ");
+  
+    const { email, password } = req.body;
+
+ 
+  // check if admin exists
+  const user = await User.findOne({ email });
+  if (!user) {
+  res.status(400).send("email déja existe");
   }
-  if (req.body.password != req.body.password2) {
-    res.status(400).send(" password not match");
+   
+  // check if role exists
+  const role = await Role.findOne({ email });
+  if (role) {
+  res.status(400).send("role not found");
   }
 
-  const { body } = req;
-  User.findOne({ email: body.email }).then((e) => {
-    // console.log('user=> '+e)
-    if (!e) {
-      Role.findOne({ type: body.role }).then((myRole) => {
-        if (myRole) {
-          // console.log(myRole)
-          body.roleid = myRole._id;
-          const token = jwt.sign({ id: User._id }, process.env.SECRET);
-          body.token = token;
-          bcrypt
-            .hash(body.password, 10)
-            .then((hashPassword) => {
-              body.password = hashPassword;
+   // check if password matches
+   const isMatch = await bcrypt.compare(password, user.password);
+   if (!isMatch) {
+     let error = new Error('Invalid credentials');
+     error.status = 400;
+     throw error;
+   }
+//   const { body } = req;
+//   User.findOne({ email: body.email }).then((e) => {
+//     // console.log('user=> '+e)
+//     if (!e) {
+//       Role.findOne({ type: body.role }).then((myRole) => {
+//         if (myRole) {
+//           // console.log(myRole)
+//           body.roleid = myRole._id;
+//           const token = jwt.sign({ id: User._id }, process.env.SECRET);
+//           body.token = token;
+//           bcrypt
+//             .hash(body.password, 10)
+//             .then((hashPassword) => {
+//               body.password = hashPassword;
 
-              User.create({ ...body })
-                .then(() => {
-                  res.status(201).send("created");
-                })
-                .catch(() => {
-                  res.status(400).send("not created // something woring ");
-                });
-            })
-            .catch(() => {
-              res.send("error in hash");
-            });
-        } else {
-          res.status(400).send("can not create //role not existe");
-        }
-      });
-    } else {
-      res.status(400).send("can not create // email déja existe");
-    }
-  });
+//               User.create({ ...body })
+//                 .then(() => {
+//                   res.status(201).send("created");
+//                 })
+//                 .catch(() => {
+//                   res.status(400).send("not created // something woring ");
+//                 });
+//             })
+//             .catch(() => {
+//               res.send("error in hash");
+//             });
+//         } else {
+//           res.status(400).send("can not create //role not existe");
+//         }
+//       });
+//     } else {
+//       res.status(400).send("can not create // email déja existe");
+//     }
+//   });
 };
 module.exports = {
   login,
