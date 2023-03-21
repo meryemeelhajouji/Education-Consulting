269ef775cbce6731a48cd1eef3cd0f556f1fc31c
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2023-03-21 14:10:52 +0100

    creact routing in frontend

---
+++ client/app-web/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home ";
+
+function App() {
+  return (
+    <Router>
+      <div className="">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/app-web/src/pages/Home .js
@@ -0,0 +1,10 @@
+import React from "react";
+function Home() {
+  return (
+    <>
+      <p>hello</p>
+    </>
+  );
+}
+
+export default Home;
